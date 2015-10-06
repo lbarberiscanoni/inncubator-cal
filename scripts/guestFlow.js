3291324cbc0e0b108f878a48b38b618a2e63ae53
lbarberiscanoni <hllbck7@gmail.com>
2015-10-06 16:53:03 -0700

    formatted the dates in teh gustflow page

--- scripts/guestFlow.js
+++ scripts/guestFlow.js
@@ -2,8 +2,8 @@ var allData = new Firebase("https://inncubator-booking.firebaseio.com");
 
 $(document).ready(function() {
     var url = window.location;
-    //var houseSelected = url.toString().split("/")[3].split("_")[0];
-    var houseSelected = url.toString().split("/")[4].split("_")[0];
+    var houseSelected = url.toString().split("/")[3].split("_")[0];
+    //var houseSelected = url.toString().split("/")[4].split("_")[0];
     $("#propertyName").html("<h1 class='text-center'>" + houseSelected + " checkins</h1>");
 
     //let's get today's date in order to make it the default
@@ -24,8 +24,10 @@ $(document).ready(function() {
     allData.orderByChild("start").on("child_added", function(snapshot) {
         var guest = snapshot.val();
         var guestStart = guest.start.split("-")[1];
+        var formattedDate = new Date(guest.start);
+        var formattedDate = formattedDate.toString().split(" ")[1] + " " + formattedDate.toString().split(" ")[2];
         if (guest.location == houseSelected && guestStart == month) {
-            $("#guestFlow").append("<button class='btn btn-default'>" + guest.title + "<br>" + guest.start + "</button><br><br>");
+            $("#guestFlow").append("<button class='btn btn-default'>" + guest.title + "<br>" + formattedDate + "</button><br><br>");
             var lol = $("#guestFlow button:last");
             var nextDay = parseInt(todaysDate.split("-")[2]) + 1;
             if (guest.start == todaysDate) {
