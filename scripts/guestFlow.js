d1c859b6f00222f93576dfb3b4efa6b902bab47c
lbarberiscanoni <hllbck7@gmail.com>
2015-11-22 12:45:15 -0500

    added the checkout view to snapshot

--- scripts/guestFlow.js
+++ scripts/guestFlow.js
@@ -21,51 +21,72 @@ $(document).ready(function() {
     };
     var todaysDate = year.toString() + "-" + month.toString() + "-" + day.toString();
     console.log(month);
-    var showCheckinsThisMonth = function(yearToAnalyze, monthToAnalyze) {
-        allData.orderByChild("start").on("child_added", function(snapshot) {
-            var guest = snapshot.val();
-            var guestYear = guest.start.split("-")[0];
-            var guestMonth = guest.start.split("-")[1];
-            
-            //let's get it in the right format
-            var guestStart = guest.start.split("-")[1];
-            var formattedDate = new Date(guest.start);
-            var formattedDate = formattedDate.toString().split(" ")[1] + " " + formattedDate.toString().split(" ")[2];
+    $("#flowDirection").change(function() {
+        $("#guestFlow").empty();
 
-            //let's showt he guests this month
-            if (guest.location == houseSelected && guestYear == yearToAnalyze && guestMonth == monthToAnalyze) {
-                $("#guestFlow").append("<button class='btn btn-default'>" + guest.title + "<br>" + formattedDate + "</button><br><br>");
+        var flowDirection = $("#flowDirection").val();
+        if (flowDirection == "Check Ins") {
+            var showCheckinsThisMonth = function(yearToAnalyze, monthToAnalyze) {
+                allData.orderByChild("start").on("child_added", function(snapshot) {
+                    var guest = snapshot.val();
+                    var guestYear = guest.start.split("-")[0];
+                    var guestMonth = guest.start.split("-")[1];
+                    
+                    //let's get it in the right format
+                    var guestStart = guest.start.split("-")[1];
+                    var formattedDate = new Date(guest.start);
+                    var formattedDate = formattedDate.toString().split(" ")[1] + " " + formattedDate.toString().split(" ")[2];
 
-                //let's mark if someone already showed up or not
-                var lol = $("#guestFlow button:last");
-                var nextDay = parseInt(todaysDate.split("-")[2]) + 1;
-                if (guest.start == todaysDate) {
-                    lol.css("background-color", "#7EB6FF");
-                } else if (parseInt(guest.start.split("-")[2]) == nextDay) {
-                    lol.css("background-color", "#FAFAD2");
-                } else {
-                    lol.addClass("disabled");
-                };
+                    //let's showt he guests this month
+                    if (guest.location == houseSelected && guestYear == yearToAnalyze && guestMonth == monthToAnalyze) {
+                        $("#guestFlow").append("<button class='btn btn-default'>" + guest.title + "<br>" + formattedDate + "</button><br><br>");
+
+                        //let's mark if someone already showed up or not
+                        var lol = $("#guestFlow button:last");
+                        var nextDay = parseInt(todaysDate.split("-")[2]) + 1;
+                        if (guest.start == todaysDate) {
+                            lol.css("background-color", "#7EB6FF");
+                        } else if (parseInt(guest.start.split("-")[2]) == nextDay) {
+                            lol.css("background-color", "#FAFAD2");
+                        } else {
+                            lol.addClass("disabled");
+                        };
+                    };
+                });
             };
-        });
-    };
-    showCheckinsThisMonth(year, month);
-    var showCheckinsFromToday = function() {
-        allData.orderByChild("start").on("child_added", function(snapshot) {
-            var guest = snapshot.val();
-            var guestStart = guest.start.split("-")[1];
-            var formattedDate = new Date(guest.start);
-            var formattedDate = formattedDate.toString().split(" ")[1] + " " + formattedDate.toString().split(" ")[2];
-            if (guest.location == houseSelected && guestStart == month && guest.start > todaysDate) {
-                $("#guestFlow").append("<button class='btn btn-default'>" + guest.title + "<br>" + formattedDate + "</button><br><br>");
-                var lol = $("#guestFlow button:last");
-                var nextDay = parseInt(todaysDate.split("-")[2]) + 1;
-                if (guest.start == todaysDate) {
-                    lol.css("background-color", "#7EB6FF");
-                } else if (parseInt(guest.start.split("-")[2]) == nextDay) {
-                    lol.css("background-color", "#FAFAD2");
-                };
+            showCheckinsThisMonth(year, month);
+        } else if (flowDirection == "Check Outs") {
+            var showCheckoutsThisMonth = function(yearToAnalyze, monthToAnalyze) {
+                allData.orderByChild("start").on("child_added", function(snapshot) {
+                    var guest = snapshot.val();
+                    var guestYear = guest.end.split("-")[0];
+                    var guestMonth = guest.end.split("-")[1];
+                    
+                    //let's get it in the right format
+                    var guestEnd = guest.end.split("-")[1];
+                    var formattedDate = new Date(guest.end);
+                    var formattedDate = formattedDate.toString().split(" ")[1] + " " + formattedDate.toString().split(" ")[2];
+
+                    //let's showt he guests this month
+                    if (guest.location == houseSelected && guestYear == yearToAnalyze && guestMonth == monthToAnalyze) {
+                        $("#guestFlow").append("<button class='btn btn-default'>" + guest.title + "<br>" + formattedDate + "</button><br><br>");
+
+                        //let's mark if someone already showed up or not
+                        var lol = $("#guestFlow button:last");
+                        var nextDay = parseInt(todaysDate.split("-")[2]) + 1;
+                        if (guest.start == todaysDate) {
+                            lol.css("background-color", "#7EB6FF");
+                        } else if (parseInt(guest.start.split("-")[2]) == nextDay) {
+                            lol.css("background-color", "#FAFAD2");
+                        } else {
+                            lol.addClass("disabled");
+                        };
+                    };
+                });
             };
-        });
-    };
+            showCheckoutsThisMonth(year, month);
+        } else {
+            console.log("problem with flow direction");
+        };
+    });
 });
