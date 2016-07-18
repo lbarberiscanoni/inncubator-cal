c069befe77001ebb37304e1b78fcfc0f0a9e5fd6
lbarberiscanoni <hllbck7@gmail.com>
2016-07-18 13:14:35 -0700

    added another room at webster

--- scripts/main.js
+++ scripts/main.js
@@ -78,6 +78,14 @@ $(document).ready(function() {
                                 var HARDCODED_MAX_EVENT_LIMIT = 20;
                                 window.HARDCODED_MAX_EVENT_LIMIT = HARDCODED_MAX_EVENT_LIMIT;
                                 break;
+                            case "webster-girls-room":
+                                var HARDCODED_MAX_EVENT_LIMIT = 4;
+                                window.HARDCODED_MAX_EVENT_LIMIT = HARDCODED_MAX_EVENT_LIMIT;
+                                break;
+                            case "webster-private":
+                                var HARDCODED_MAX_EVENT_LIMIT = 1;
+                                window.HARDCODED_MAX_EVENT_LIMIT = HARDCODED_MAX_EVENT_LIMIT;
+                                break;
                             case "stanford-inn":
                                 var HARDCODED_MAX_EVENT_LIMIT = 12;
                                 window.HARDCODED_MAX_EVENT_LIMIT = HARDCODED_MAX_EVENT_LIMIT;
